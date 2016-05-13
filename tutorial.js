d4c6976d4246a2e001fab96a90bd9953ae6e8e8f
kytiken <kytiken@gmail.com>
2016-05-13 22:21:42 +0900

    using comment class

--- tutorial.js
+++ tutorial.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
       return (
         React.createElement('div', {className: "commentBox"},
           "Hello, world! I am a CommentBox.",
-          React.createElement('p', {className: "comment"}, "this is comment")
+          React.createElement('p', {className: "comment"}, "this is comment"),
+          React.createElement(Comment, null, null)
         )
       );
     }
